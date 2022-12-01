bbf7adc934e5cb1c9fe2640919545c1cdad29d90
DavidGermanLabandera <jdgerman@hotmail.com>
2022-11-30 20:13:50 -0700

    c

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,9 @@
 import { BranchOffice } from "../interfaces/branchOffice";
+<<<<<<< .merge_file_ahtze1
 import { UserBranchOfficeSeller } from "../views/userBranchOfficeSeller/createUserBranchOfficeSeller"
+=======
+import { UserBranchOfficeSeller } from "../views/UserBranchOfficeSeller/CreateUserBranchOfficeSeller"
+>>>>>>> .merge_file_hPAnl1
 
 export const initBranch: BranchOffice = {
   userAdmin: "",
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,10 +1,18 @@
 import { PathRouteProps } from 'react-router-dom';
+<<<<<<< .merge_file_GWL3WS
 import Branches from '../views/branches';
 import CreateBranch from '../views/branches/CreateBranch';
 import LandingPage from '../views/landingPage';
 import Profile from '../views/profile';
 import UserBranchOfficeSeller from '../views/userBranchOfficeSeller';
 import CreateUserBranchOfficeSeller from '../views/userBranchOfficeSeller/createUserBranchOfficeSeller';
+=======
+import Branches from '../views/Branches';
+import CreateBranch from '../views/Branches/CreateBranch';
+import LandingPage from '../views/LandingPage';
+import UserBranchOfficeSeller from '../views/UserBranchOfficeSeller';
+import CreateUserBranchOfficeSeller from '../views/UserBranchOfficeSeller/CreateUserBranchOfficeSeller';
+>>>>>>> .merge_file_cvbc2S
 
 const routes: PathRouteProps[] = [
   {
