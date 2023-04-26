a93f564565a12a8db97528085638d73ff8894c7c
fedeWax <cha_lino_23@hotmail.com>
2023-04-25 22:52:25 -0700

    mejora estilo tabla y menu lateral

--- src/components/menu/rowHeader.tsx
+++ src/components/menu/rowHeader.tsx
@@ -17,15 +17,17 @@ const RowHeader: FC<Props> = ({ collapsed }) => {
   return (
     <Row
       justify="center"
-      style={screens.xs ?
-        { textAlign: "center" } :
-        { textAlign: "center", backgroundColor: "#304878", marginTop: -19, paddingTop: 20, paddingBottom: 20, borderRadius: "0px 0px 8px 8px" }}
+      style={
+        screens.xs ?
+          { textAlign: "center" } :
+          { textAlign: "center", backgroundColor: "#304878", margin: 10, paddingTop: 20, paddingBottom: 20, borderRadius: "8px" }
+      }
     >
       <Avatar style={{ backgroundColor: "#87D068" }} size={collapsed ? 48 : 64} icon={<UserOutlined />} />
       {
-        !collapsed && <div style={screens.xs ?{ color: "black" }:{ color: "white" }}>
-          <div style={{ marginTop: 20 }}>
-            <b>{userAdmin?.email.toUpperCase()}</b>
+        !collapsed && <div style={screens.xs ? { color: "black" } : { color: "white" }}>
+          <div style={{ margin: 10 }}>
+            <b>{userAdmin?.email}</b>
           </div>
           <div>
             {userAdmin?.role}
